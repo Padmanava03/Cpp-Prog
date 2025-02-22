fa0c66cdc12ab0d0aa833602a15a8f851283ad3c
Padwa <biswas.padmanava03@gmail.com>
2025-02-22 09:40:36 +0530

    started oops in C++

---
+++ OOP/oops.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Teacher{
+    private:
+        double salary;
+    public:
+        string name;
+        string dept;
+        string subject;
+
+        Teacher(string name, string dept, string subject, double salary){
+            this->name = name;
+            this->dept = dept;
+            this->subject = subject;
+            setSalary(salary);
+        }
+
+        double getSalary(){
+            return (*this).salary;
+        }
+
+        void setSalary(double newSalary){
+            this->salary = newSalary;
+        }
+
+        void info(){
+            cout << name << " is a teacher of " << dept << " department and teaches " << subject << " and earns " << salary << " per month." << endl;
+        }
+};
+
+int main(){
+    Teacher T1("Mr. John", "CSE", "OOP", 50000);
+    Teacher T2("Mr. Doe", "EEE", "Circuit Theory", 45000);
+
+    T1.info();
+    T2.info();
+
+    return 0;
+}
